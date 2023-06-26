8c86047c11d4a194aebae7711aaea8d77f405b53
DaraniShri <daranirameshbabu@gmail.com>
2023-06-26 18:30:20 +0530

    adjust(student):form design added

--- create_student.php
+++ create_student.php
@@ -9,60 +9,76 @@
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+          <style>
+          .container{
+               margin-top:150px;
+               border:2px solid black;
+          }
+     </style>
      </head>
      <body>
-     <form class="form-horizontal" action="store_student.php" method="POST">
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="user_name">Username:</label>
-               <div class="col-sm-10">
-                    <input type="text" class="form-control" id="user_name" placeholder="Enter username" name="user_name">
+     <div class="container">
+          <div class="row">
+               <div class="col-2">
                </div>
-          </div>
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="pwd">Password:</label>
-               <div class="col-sm-10">          
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+               <div class="col-8">
+                    <form class="form-horizontal" action="store_student.php" method="POST">
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="user_name">Username:</label>
+                              <div class="col-sm-4">
+                                   <input type="text" class="form-control" id="user_name" placeholder="Enter username" name="user_name">
+                              </div>
+                         </div>
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="password">Password:</label>
+                              <div class="col-sm-4">          
+                                   <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+                              </div>
+                         </div>
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="email">Email:</label>
+                              <div class="col-sm-4">
+                                   <input type="email" class="form-control" id="email" placeholder="Enter email" name="e_mail">
+                              </div>
+                         </div>
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="email">Gender:</label>
+                              <div class="col-sm-4">
+                                   <input type="radio" class="form-check-input" name="gender" value="male">male
+                                   <input type="radio" class="form-check-input" name="gender" value="female">female
+                              </div> 
+                         </div>  
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="email">Skills:</label>
+                              <div class="col-sm-4">
+                                   <input type="checkbox" name="skills[]" value="java">java
+                                   <input type="checkbox" name="skills[]" value="js">js
+                                   <input type="checkbox" name="skills[]" value="php">php
+                                   <input type="checkbox" name="skills[]" value="css">css
+                              </div>
+                         </div>
+                         <div class="form-group">
+                              <label class="control-label col-sm-2" for="email">City:</label>
+                              <div class="col-sm-4">
+                              <select id="cities" name="cities">
+                                        <option value="madurai">Madhurai</option>
+                                        <option value="chennai">Chennai</option>
+                                        <option value="sivakasi">Sivakasi</option>
+                                        <option value="sivakasi">Theni</option>
+                                        <option value="sivakasi">Kaasi</option>
+                                   </select> 
+                              </div>
+                         </div>
+                         <div class="form-group">        
+                              <div class="col-sm-offset-2 col-sm-4">
+                                   <input class="btn btn-success" type="submit" value="submit" name="submit">
+                              </div>
+                         </div>
+                    </form> 
                </div>
+               <div class="col-2">
+               </div>     
           </div>
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="email">Email:</label>
-               <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="e_mail">
-               </div>
-          </div>
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="email">Gender:</label>
-               <div class="col-sm-10">
-                    <input type="radio" class="form-check-input" name="gender" value="male">male
-                    <input type="radio" class="form-check-input" name="gender" value="female">female
-               </div> 
-          </div>  
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="email">Skills:</label>
-               <div class="col-sm-10">
-                    <input type="checkbox" name="skills[]" value="java">java
-                    <input type="checkbox" name="skills[]" value="js">js
-                    <input type="checkbox" name="skills[]" value="php">php
-                    <input type="checkbox" name="skills[]" value="css">css
-               </div>
-          </div>
-          <div class="form-group">
-               <label class="control-label col-sm-2" for="email">City:</label>
-               <div class="col-sm-10">
-                   <select id="cities" name="cities">
-                         <option value="madurai">Madhurai</option>
-                         <option value="chennai">Chennai</option>
-                         <option value="sivakasi">Sivakasi</option>
-                         <option value="sivakasi">Theni</option>
-                         <option value="sivakasi">Kaasi</option>
-                    </select> 
-               </div>
-          </div>
-          <div class="form-group">        
-               <div class="col-sm-offset-2 col-sm-10">
-                    <input class="btn btn-success" type="submit" value="submit" name="submit">
-               </div>
-          </div>
-     </form>               
+     </div>              
      </body> 
 </html>
--- edit_student.php
+++ edit_student.php
@@ -31,60 +31,76 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
 <head>
      <title>Student Details</title>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+     <style>
+          .container{
+               margin-top:150px;
+               border:2px solid black;
+          }
+     </style>
 </head>
 <body>
-     <form class="form-horizontal" action="update_student.php"  method="POST">
-          <input type="hidden" name="id" value="<?php echo $id; ?>">
-          <div class="form-group">
-          <label class="control-label col-sm-2" for="user_name">Username:</label>
-          <div class="col-sm-10">
-               <input type="text" class="form-control" id="user_name" placeholder="Enter username" name="user_name">
+<div class="container">
+     <div class="row">
+          <div class="col-4">
           </div>
-     </div>
-     <div class="form-group">
-          <label class="control-label col-sm-2" for="pwd">Password:</label>
-          <div class="col-sm-10">          
-               <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-          </div>
-     </div>
-     <div class="form-group">
-          <label class="control-label col-sm-2" for="email">Email:</label>
-          <div class="col-sm-10">
-               <input type="email" class="form-control" id="email" placeholder="Enter email" name="e_mail">
-          </div>
-     </div>
-     <div class="form-group">
-          <label class="control-label col-sm-2" for="email">Gender:</label>
-          <div class="col-sm-10">
-               <input type="radio" class="form-check-input" name="gender" value="male" <?php if ($gender != "female") echo "checked"; ?>>male
-               <input type="radio" class="form-check-input" name="gender" <?php if ($gender == "female") echo "checked"; ?>>female
+          <div class="col-4">
+               <form class="form-horizontal" action="update_student.php"  method="POST">
+                    <input type="hidden" name="id" value="<?php echo $id; ?>">
+                    <div class="form-group">
+                    <label class="control-label col-sm-2" for="user_name">Username:</label>
+                    <div class="col-sm-4">
+                         <input type="text" class="form-control" id="user_name" placeholder="Enter username" name="user_name" value="<?php echo $name;?>">
+                    </div>
+               </div>
+               <div class="form-group">
+                    <label class="control-label col-sm-2" for="pwd">Password:</label>
+                    <div class="col-sm-4">          
+                         <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd" value="<?php echo $password;?>">
+                    </div>
+               </div>
+               <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Email:</label>
+                    <div class="col-sm-4">
+                         <input type="email" class="form-control" id="email" placeholder="Enter email" name="e_mail" value="<?php echo $email;?>">
+                    </div>
+               </div>
+               <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Gender:</label>
+                    <div class="col-sm-4">
+                         <input type="radio" class="form-check-input" name="gender" value="male" <?php if ($gender != "female") echo "checked"; ?>>male
+                         <input type="radio" class="form-check-input" name="gender" <?php if ($gender == "female") echo "checked"; ?>>female
+                    </div> 
+               </div> 
+               <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Skills:</label>
+                    <div class="col-sm-4">
+                         <input type="checkbox" name="skills[]" value="java" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>java
+                         <input type="checkbox" name="skills[]" value="js" <?php echo (in_array("js",$total_skills) ? 'checked' : ''); ?>>js
+                         <input type="checkbox" name="skills[]" value="php" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>php
+                         <input type="checkbox" name="skills[]" value="css" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>css
+                    </div>
+               </div>
+               <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">City:</label>
+                    <div class="col-sm-4">
+                         <select id="cities" name="cities">
+                              <option value="madurai" <?=$city == 'madurai' ? ' selected="selected"' : '';?>>Madhurai</option>
+                              <option value="chennai" <?=$city == 'chennai' ? ' selected="selected"' : '';?>>Chennai</option>
+                              <option value="sivakasi" <?=$city == 'sivakasi' ? ' selected="selected"' : '';?>>Sivakasi</option>
+                              <option value="theni" <?=$city == 'theni' ? ' selected="selected"' : '';?>>Theni</option>
+                         </select> 
+                    </div>
+               </div>
+               <div class="form-group">        
+                    <div class="col-sm-offset-2 col-sm-10">
+                         <input class="btn btn-success" type="submit" value="update" name="submit">
+                    </div>
+               </div>
+               </form>
           </div> 
-     </div> 
-     <div class="form-group">
-          <label class="control-label col-sm-2" for="email">Skills:</label>
-          <div class="col-sm-10">
-               <input type="checkbox" name="skills[]" value="java" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>java
-               <input type="checkbox" name="skills[]" <?php echo (in_array("js",$total_skills) ? 'checked' : ''); ?>>js
-               <input type="checkbox" name="skills[]" value="php" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>php
-               <input type="checkbox" name="skills[]" value="css" <?php echo (in_array("java",$total_skills) ? 'checked' : ''); ?>>css
+          <div class="col-4">
           </div>
      </div>
-     <div class="form-group">
-          <label class="control-label col-sm-2" for="email">City:</label>
-          <div class="col-sm-10">
-               <select id="cities" name="cities">
-                    <option value="madurai" <?=$city == 'madurai' ? ' selected="selected"' : '';?>>Madhurai</option>
-                    <option value="chennai" <?=$city == 'chennai' ? ' selected="selected"' : '';?>>Chennai</option>
-                    <option value="sivakasi" <?=$city == 'sivakasi' ? ' selected="selected"' : '';?>>Sivakasi</option>
-                    <option value="theni" <?=$city == 'theni' ? ' selected="selected"' : '';?>>Theni</option>
-               </select> 
-          </div>
-     </div>
-     <div class="form-group">        
-          <div class="col-sm-offset-2 col-sm-10">
-               <input class="btn btn-success" type="submit" value="update" name="submit">
-          </div>
-     </div>
-     </form> 
+</div>
 </body> 
 </html>
